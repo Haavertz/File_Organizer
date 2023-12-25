9de97da4d87ee9f1539bbeb7c01cb9538027808f
Gleison <gleisonsenac06@gmail.com>
2023-12-25 18:16:01 -0300

    Update Final

--- Calculator/Main.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QApplication
-from App import MainWindow
-import sys
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    windown = MainWindow()
-    windown.show()
-    app.exec()
--- HASH/main.py
+++
@@ -1,9 +0,0 @@
-from app import MainWindow
-from PySide6.QtWidgets import QApplication
-import sys
-
-if __name__ == "__main__":    
-    App = QApplication(sys.argv)
-    Janela = MainWindow()
-    Janela.show()
-    App.exec()
